c7f5d23c2cd74867987787b2504651df297372bb
ChrisNotDefined <christopherapstarwars3@gmail.com>
2020-12-06 15:53:55 -0600

    DestiniesController

--- app/src/main/java/com/devchrisap/apptourism/Controllers/DestiniesController.kt
+++ app/src/main/java/com/devchrisap/apptourism/Controllers/DestiniesController.kt
@@ -1,5 +1,50 @@
 package com.devchrisap.apptourism.Controllers
 
-class DestiniesController {
+import android.util.Log
+import com.devchrisap.apptourism.Entities.Destiny
+import com.devchrisap.apptourism.Interfaces.RestClient
+import com.devchrisap.apptourism.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+class DestiniesController {
+    var destiniesList: List<Destiny> = emptyList()
+    var baseUrl: String = "http://localhost:3000"
+
+    fun getDestinies() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val restClient: RestClient = retrofit.create(RestClient::class.java)
+
+        val call: Call<List<Destiny>> = restClient.cargarDestinosDe("1")
+
+        call.enqueue(object : Callback<List<Destiny>> {
+            override fun onResponse(call: Call<List<Destiny>>, response: Response<List<Destiny>>) {
+                when (response.code()) {
+                    200 -> {
+                        destiniesList = emptyList()
+                        destiniesList = response.body()!!
+                        Log.i("Entro http 200", destiniesList.toString())
+                    }
+                    401 -> {
+
+                    }
+                    else -> {
+
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Destiny>>, t: Throwable) {
+                Log.e("Failed getdestinies", t.toString())
+            }
+        })
+
+    }
 }
--- app/src/main/java/com/devchrisap/apptourism/Interfaces/RestClient.kt
+++ app/src/main/java/com/devchrisap/apptourism/Interfaces/RestClient.kt
@@ -1,15 +1,31 @@
 package com.devchrisap.apptourism.Interfaces
 
 import com.devchrisap.apptourism.Entities.City
+import com.devchrisap.apptourism.Entities.Destiny
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface RestClient {
 
+    /**
+     *  Obtener todas las ciudades
+     */
     @GET("cities") //localhost:3000/cities
     fun cargarCiudades(): Call<List<City>>
 
-    //@GET("/by-city/{cityId}")
+    /**
+     * Para obtener el listado de destinos de una ciudad
+     * @param cityId El ID de la ciudad
+     * */
+    @GET("by-city/{cityId}")
+    fun cargarDestinosDe(@Path("cityId") cityId: String): Call<List<Destiny>>
 
+    /**
+     * Para obtener el destino con ese ID
+     * @param destinyId ID del Destino
+     */
+    @GET("by-id/{destinyId}")
+    fun cargarDestino(@Path("destinyId") destinyId: String): Call<Destiny>
 }
