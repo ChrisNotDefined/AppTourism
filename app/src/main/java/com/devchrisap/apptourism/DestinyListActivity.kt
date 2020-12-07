96990fa556d512434d83fa9893c44fe404f6b165
Javier-debug <madrajavi@gmail.com>
2020-12-07 15:58:49 -0600

    Funcionalidad favoritos

--- app/src/main/java/com/devchrisap/apptourism/DestinyListActivity.kt
+++ app/src/main/java/com/devchrisap/apptourism/DestinyListActivity.kt
@@ -1,23 +1,31 @@
 package com.devchrisap.apptourism
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.devchrisap.apptourism.Controllers.DestiniesController
+import com.devchrisap.apptourism.Entities.User
+import com.devchrisap.apptourism.Entities.userFavorites
+import com.devchrisap.apptourism.Models.DbUserFavorites
+import com.devchrisap.apptourism.Models.DbUsers
 import io.sulek.ssml.SSMLLinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.lista_destinos.*
 import kotlinx.android.synthetic.main.lista_destinos.view.*
 
+lateinit var cityId: String
 class DestinyListActivity : Fragment() {
     lateinit var destiniesController: DestiniesController
     private lateinit var layoutManager: RecyclerView.LayoutManager
-    lateinit var cityId: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
